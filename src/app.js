0da4ab18105182fefac26422d7be18fe7986e6fe
Valdir Coxev [Home] <valdircoxevdev@gmail.com>
2020-05-12 09:54:15 -0400

    finished challenge

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -12,22 +12,52 @@ const repositories = [];
 
 app.get("/repositories", (request, response) => {
   // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
   // TODO
+  const { title, url, techs } = request.body;
+  repositories.push({ id: uuid(), title, url, techs, likes: 0 });
+  return response.status(201).json(repositories[repositories.length - 1]);
 });
 
 app.put("/repositories/:id", (request, response) => {
   // TODO
+  const {id} = request.params;
+  const {title, url, techs} = request.body;
+  const repository = repositories.find(item => item.id === id);
+  if(!repository){
+    return response.status(400).json({message: 'Repository not found'});
+  }
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+  return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
   // TODO
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(item => item.id === id)
+  if(repositoryIndex !== -1){
+    repositories.splice(repositoryIndex, 1);
+    return response.status(204).send();
+  }else {
+    return response.status(400).json({message: 'Repository not found!'});
+  }
 });
 
 app.post("/repositories/:id/like", (request, response) => {
   // TODO
+  const {id} = request.params;
+  const repository = repositories.find(item => item.id === id);
+  if(!repository){
+    return response.status(400).json({message: 'Repository not found'});
+  }
+  repository.likes += 1;
+  return response.json(repository);
 });
 
 module.exports = app;
